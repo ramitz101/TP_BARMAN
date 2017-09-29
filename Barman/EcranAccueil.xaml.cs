f72a262bd0ba8bdb3c02e5b311bf12bed1fe7b74
ramitz101 <ramitz101@gmail.com>
2017-09-29 17:03:04 -0400

    test3

---
+++ Barman/EcranAccueil.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Barman
+{
+    /// <summary>
+    /// Logique d'interaction pour EcranAccueil.xaml
+    /// </summary>
+    public partial class EcranAccueil : UserControl
+    {
+        private EcranInventaire Ecraninventaire { get; set; }
+        public EcranAccueil()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInventaire_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)System.Windows.Application.Current.MainWindow).GrdPrincipale.Children.Clear();
+
+            Ecraninventaire = new EcranInventaire();
+            ((MainWindow)System.Windows.Application.Current.MainWindow).GrdPrincipale.Children.Add(Ecraninventaire);
+
+
+        }
+
+        private void btnEmploye_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVente_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCommande_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFormulaireBouteille_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Barman/EcranCommande.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Barman
+{
+    /// <summary>
+    /// Logique d'interaction pour EcranCommande.xaml
+    /// </summary>
+    public partial class EcranCommande : UserControl
+    {
+        public EcranCommande()
+        {
+            InitializeComponent();
+            tbcOnglet.SelectedItem = tbiCommande;
+            tbiCommande.IsSelected = true;
+
+        }
+
+       
+       
+
+        private void tbcOnglet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+         
+            if (tbcOnglet.SelectedItem == tbiInventaire)
+            {
+                ((MainWindow)System.Windows.Application.Current.MainWindow).GrdPrincipale.Children.Clear();
+                EcranInventaire EI = new EcranInventaire();
+                ((MainWindow)System.Windows.Application.Current.MainWindow).GrdPrincipale.Children.Add(EI);
+
+            }
+            else if (!tbiCommande.IsSelected && tbcOnglet.SelectedItem == tbiCommande)
+            {
+
+
+                ((MainWindow)System.Windows.Application.Current.MainWindow).GrdPrincipale.Children.Clear();
+                EcranCommande EC = new EcranCommande();
+                ((MainWindow)System.Windows.Application.Current.MainWindow).GrdPrincipale.Children.Add(EC);
+               
+            }
+        }
+    }
+}
